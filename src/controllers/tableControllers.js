af11dae1009fd6b5433c0801898b77257963a7d1
Belén Peluffo <111821624+BelenPeluffo@users.noreply.github.com>
2024-06-19 13:00:21 -0300

    refactor: deletes comments.

--- src/controllers/tableControllers.js
+++ src/controllers/tableControllers.js
@@ -1,17 +1,8 @@
 import { IDOLS } from "../constants/TableData.js";
 
 export const getTableData = (req, res) => {
-  console.log("Ejecutando getTableData...");
-  console.log("req.query?", req.query);
   if (req.query.sun || req.query.moon || req.query.rising || req.query.name) {
-    console.log("Filtrando datos... req.query?", req.query);
     const filteredData = IDOLS.filter((idol) => {
-      //   return (
-      //     (req.query.sun && idol.sun === Number(req.query.sun)) ||
-      //     (req.query.moon && idol.moon === Number(req.query.moon)) ||
-      //     (req.query.rising && idol.rising === Number(req.query.rising)) ||
-      //     (req.query.name && idol.name === req.query.name)
-      //   );
       return (req.query.name === "" ||
         idol.name.toLowerCase().includes(req.query.name) ||
         req.query.name === null) &&
